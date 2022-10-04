e1894818baf827d79ec9a9c44f4b2bae71dcd98f
jmamadeu <jmamadeu2000@gmail.com>
2022-10-04 03:50:52 +0100

    chore: add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button, Input, Space, Typography } from "antd";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+
+    console.log(event.currentTarget.email.value);
+    console.log(event.currentTarget.password.value);
+  };
+
+  const isLoginButtonDisabled = !email.trim() || !password.trim()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="h-screen flex justify-center items-center flex-col">
+      <Typography.Title>Login</Typography.Title>
+
+      <form className="w-80" onSubmit={handleSubmit}>
+        <Space direction="vertical" size="large">
+          <Input
+            name="email"
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            className="w-full rounded-md"
+          />
+
+          <Input.Password
+            name="password"
+            placeholder="enter your Password"
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+            className="w-80 rounded-md"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <Button datatype="submit" className="rounded-md" htmlType="submit" disabled={isLoginButtonDisabled}>
+            Log in
+          </Button>
+        </Space>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
